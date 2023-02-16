cbec5b45db0f12b3a36ff5d9fa2327e3cdb32291
Tuan Kiet Nguyen <45098371+luckykiet@users.noreply.github.com>
2023-02-16 09:01:15 +0100

    simplier state using immer

--- src/stores/ZustandStores.js
+++ src/stores/ZustandStores.js
@@ -1,50 +1,68 @@
 import { nanoid } from 'nanoid';
 import { create } from 'zustand';
+import produce from 'immer';
+import { useCallback } from 'react';
 
 const useStore = create((set) => ({
-    settings: {}, setSettings: (settings) => set(() => ({ settings })),
-    isSidebarShowed: false, setIsSidebarShowed: (isSidebarShowed) => set(() => ({ isSidebarShowed })),
-    cartItems: [], addToCartItem: (item, quantity) => set((state) => {
-        const newCartItems = [...state.cartItems];
-        const existingItemIndex = newCartItems.findIndex((i) => i.ean === item.ean);
-        if (existingItemIndex >= 0) {
-            newCartItems[existingItemIndex].quantity += quantity || 1;
-        } else {
-            newCartItems.push({ id: nanoid(), ean: item.ean, name: item.name, price: item.price, quantity: quantity || 1 });
-        }
-        return { cartItems: newCartItems };
-    }),
-    decrementCartItem: (id) => set((state) => {
-        const newCartItems = [...state.cartItems];
-        const index = newCartItems.findIndex((i) => i.id === id);
-        newCartItems[index].quantity -= 1;
-        if (!newCartItems[index].quantity) {
-            newCartItems.splice(index, 1);
-        }
-        return { cartItems: newCartItems };
-    }),
-    incrementCartItem: (id) => set((state) => {
-        const newCartItems = [...state.cartItems];
-        const index = newCartItems.findIndex((i) => i.id === id);
-        newCartItems[index].quantity += 1;
-        return { cartItems: newCartItems };
-    }),
-    removeCartItem: (id) => set((state) => {
-        const newCartItems = [...state.cartItems];
-        const index = newCartItems.findIndex((i) => i.id === id);
-        newCartItems.splice(index, 1);
-        return { cartItems: newCartItems };
-    }),
+    settings: {},
+    setSettings: (settings) => set({ settings }),
+
+    isSidebarShowed: false,
+    setIsSidebarShowed: (isSidebarShowed) => set({ isSidebarShowed }),
+
+    cartItems: [],
+    addToCartItem: (item, quantity) =>
+        set(
+            produce((state) => {
+                const existingItemIndex = state.cartItems.findIndex((i) => i.ean === item.ean);
+                if (existingItemIndex >= 0) {
+                    state.cartItems[existingItemIndex].quantity += quantity || 1;
+                } else {
+                    state.cartItems.push({
+                        id: nanoid(),
+                        ean: item.ean,
+                        name: item.name,
+                        price: item.price,
+                        quantity: quantity || 1,
+                    });
+                }
+            })
+        ),
+    decrementCartItem: (id) =>
+        set(
+            produce((state) => {
+                const index = state.cartItems.findIndex((i) => i.id === id);
+                state.cartItems[index].quantity -= 1;
+                if (!state.cartItems[index].quantity) {
+                    state.cartItems.splice(index, 1);
+                }
+            })
+        ),
+    incrementCartItem: (id) =>
+        set(
+            produce((state) => {
+                const index = state.cartItems.findIndex((i) => i.id === id);
+                state.cartItems[index].quantity += 1;
+            })
+        ),
+    removeCartItem: (id) =>
+        set(
+            produce((state) => {
+                const index = state.cartItems.findIndex((i) => i.id === id);
+                state.cartItems.splice(index, 1);
+            })
+        ),
 }));
 
-export const useCartItems = () => useStore((state) => state.cartItems);
-export const useAddToCartItem = () => useStore((state) => state.addToCartItem);
-export const useRemoveCartItem = () => useStore((state) => state.removeCartItem);
-export const useDecrementCartItem = () => useStore((state) => state.decrementCartItem);
-export const useIncrementCartItem = () => useStore((state) => state.incrementCartItem);
+// Memoized selectors
+export const useCartItems = () => useStore(useCallback((state) => state.cartItems, []));
+export const useAddToCartItem = () => useStore(useCallback((state) => state.addToCartItem, []));
+export const useRemoveCartItem = () => useStore(useCallback((state) => state.removeCartItem, []));
+export const useDecrementCartItem = () => useStore(useCallback((state) => state.decrementCartItem, []));
+export const useIncrementCartItem = () => useStore(useCallback((state) => state.incrementCartItem, []));
 
-export const useSettings = () => useStore((state) => state.settings);
-export const useSetSettings = () => useStore((state) => state.setSettings);
+export const useSettings = () => useStore(useCallback((state) => state.settings, []));
+export const useSetSettings = () => useStore(useCallback((state) => state.setSettings, []));
 
-export const useIsSidebarShowed = () => useStore((state) => state.isSidebarShowed);
-export const useSetIsSidebarShowed = () => useStore((state) => state.setIsSidebarShowed);
+export const useIsSidebarShowed = () => useStore(useCallback((state) => state.isSidebarShowed, []));
+export const useSetIsSidebarShowed = () => useStore(useCallback((state) => state.setIsSidebarShowed, []));
