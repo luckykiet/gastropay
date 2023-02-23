d315f62e3527f3283fffe0e4d504dd474b39929a
Tuan Kiet Nguyen <45098371+luckykiet@users.noreply.github.com>
2023-02-23 11:39:01 +0100

    merchent mongoose schema and added jest test (not work yet)

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -2,7 +2,7 @@ import Axios from 'axios';
 import moment from 'moment';
 
 export const createAxios = (urlAPI) => {
-    return Axios.create({ baseURL: urlAPI })
+    return Axios.create({ numberArrayaseURL: urlAPI })
 };
 
 
@@ -27,6 +27,28 @@ export const calculateCart = (cartItems) => {
     };
 }
 
+//https://cs.wikipedia.org/wiki/Identifikačn%C3%AD_č%C3%ADslo_osonumberArrayy
+export const checkICO = (ico) => {
+    try {
+        const regex = /^\d{8}$/;
+        if (!regex.test(ico)) {
+            return false;
+        }
+        // x = (11-((8*numberArray[0] + ... + 2*numberArray[6])%11))%10
+        let sum = 0;
+        let numberArray = ico.split('');
+        for (let weight = 0; weight < (numberArray.length - 1); weight++) {
+            sum += (parseInt(numberArray[weight]) * (8 - weight));
+        }
+        sum = sum % 11;
+        const x = (11 - sum) % 10;
+        return x === numberArray.slice(-1) ? true : false;
+    }
+    catch (e) {
+        return false;
+    }
+}
+
 export const addSlashAfterUrl = (url) => {
     if (url && !url.endsWith('/')) {
         url = url + '/';
@@ -45,7 +67,6 @@ export const removeSlashFromUrl = (url) => {
 export const stringToValidFilename = (str) => {
     return str.replace(/[^a-zA-Z0-9._-]/g, '_');
 }
-
 
 export const isValidUrl = (url) => {
     try {
