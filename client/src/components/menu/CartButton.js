7cd2d8574e2988c9236d828148ff6d8b6327a7f2
Tuan Kiet Nguyen <45098371+luckykiet@users.noreply.github.com>
2023-03-08 17:11:40 +0100

    icon size

--- client/src/components/menu/CartButton.js
+++ client/src/components/menu/CartButton.js
@@ -6,8 +6,8 @@ import 'bulma/';
 
 export default function CartButton({ numItems, showSidebar }) {
     return (
-        <Button className="is-size-4 is-size-3-mobile" color={'primary'} onClick={showSidebar}>
-            <FontAwesomeIcon icon={faCartShopping} />
+        <Button className="is-size-5 is-size-3-mobile" color={'primary'} onClick={showSidebar}>
+            <FontAwesomeIcon size='2x' icon={faCartShopping} />
             {numItems > 0 && <span className='has-text-weight-bold'>&nbsp;{numItems}x</span>}
         </Button>
     );
--- client/src/components/menu/MenuNavbar.js
+++ client/src/components/menu/MenuNavbar.js
@@ -15,7 +15,7 @@ export default function MenuNavbar({ showSidebar }) {
     return (
         <Navbar color={'primary'} role="navigation" aria-label="menu navigation" className='is-spaced has-shadow'>
             <Brand>
-                <Item className='is-size-3 has-text-weight-bold' renderAs={Link} to={PATHS.HOME}>
+                <Item className='is-size-4 has-text-weight-bold' renderAs={Link} to={PATHS.HOME}>
                     {packageJson.app.name}
                 </Item>
             </Brand>
