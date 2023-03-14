82eb3080c87a45593f1b754421affd2e471167f0
Tuan Kiet Nguyen <45098371+luckykiet@users.noreply.github.com>
2023-03-14 23:18:38 +0100

    add try catch to controllers

--- server/functions/controllers/GuestController.js
+++ server/functions/controllers/GuestController.js
@@ -4,38 +4,42 @@ const MerchantModel = require("../models/MerchantModel");
 const TransactionController = require('./TransactionController');
 
 const getTransaction = async (req, res) => {
-    const { idTransaction } = req.params;
+    try {
+        const { idTransaction } = req.params;
 
-    const transaction = await TransactionModel.findOne({ refId: idTransaction });
-    if (!transaction) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `Transaction not found` });
+        const transaction = await TransactionModel.findOne({ refId: idTransaction });
+        if (!transaction) {
+            return res
+                .status(404)
+                .json({ success: false, msg: `Transaction not found` });
+        }
+
+        const paymentMethodName = Object.keys(transaction.paymentMethod)[0];
+        const status = transaction.paymentMethod[paymentMethodName].status;
+        if (status === 'PENDING' || status === 1 || status === 2) {
+            await TransactionController.checkPayment(idTransaction);
+        }
+
+        const restaurant = await RestaurantModel.findById(transaction.idRestaurant).select("idOwner name address").exec();
+
+        if (!restaurant) {
+            return res
+                .status(404)
+                .json({ success: false, msg: `Restaurant not found` });
+        }
+
+        const merchant = await MerchantModel.findById(restaurant.idOwner).select("ico").exec();
+
+        if (!merchant) {
+            return res
+                .status(404)
+                .json({ success: false, msg: `Merchant not found` });
+        }
+
+        return res.status(200).json({ success: true, msg: { transaction: transaction, restaurant: restaurant, merchant: merchant } });
+    } catch (error) {
+        return res.status(400).json({ success: false, msg: error });
     }
-
-    const paymentMethodName = Object.keys(transaction.paymentMethod)[0];
-    const status = transaction.paymentMethod[paymentMethodName].status;
-    if (status === 'PENDING' || status === 1 || status === 2) {
-        await TransactionController.checkPayment(idTransaction);
-    }
-
-    const restaurant = await RestaurantModel.findById(transaction.idRestaurant).select("idOwner name address").exec();
-
-    if (!restaurant) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `Restaurant not found` });
-    }
-
-    const merchant = await MerchantModel.findById(restaurant.idOwner).select("ico").exec();
-
-    if (!merchant) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `Merchant not found` });
-    }
-
-    return res.status(200).json({ success: true, msg: { transaction: transaction, restaurant: restaurant, merchant: merchant } });
 }
 
 module.exports = { getTransaction }
--- server/functions/controllers/MerchantController.js
+++ server/functions/controllers/MerchantController.js
@@ -7,28 +7,27 @@ const bcrypt = require('bcryptjs');
 const moment = require('moment');
 
 const createRestaurant = async (req, res, next) => {
-    const authorizedMerchantId = req.userId;
-    const body = req.body;
-    if (!body) {
-        return res.status(400).json({
-            success: false,
-            msg: "You must provide a restaurant",
-        });
-    }
-
-    const foundRestaurant = await RestaurantModel.findOne({
-        idOwner: ObjectId(authorizedMerchantId),
-        name: body.name
-    }).select("name").exec();
-
-    if (foundRestaurant) {
-        return res.status(400).json({
-            success: false,
-            msg: "Restaurant " + foundRestaurant.name + " already exists!",
-        });
-    }
-
     try {
+        const authorizedMerchantId = req.userId;
+        const body = req.body;
+        if (!body) {
+            return res.status(400).json({
+                success: false,
+                msg: "You must provide a restaurant",
+            });
+        }
+
+        const foundRestaurant = await RestaurantModel.findOne({
+            idOwner: ObjectId(authorizedMerchantId),
+            name: body.name
+        }).select("name").exec();
+
+        if (foundRestaurant) {
+            return res.status(400).json({
+                success: false,
+                msg: "Restaurant " + foundRestaurant.name + " already exists!",
+            });
+        }
         const restaurant = new RestaurantModel({
             ...body,
             idOwner: ObjectId(authorizedMerchantId),
@@ -48,61 +47,64 @@ const createRestaurant = async (req, res, next) => {
 };
 
 const getRestaurants = async (req, res) => {
-    const query = RestaurantModel.find({ idOwner: ObjectId(req.userId) });
-    query.sort({ createdAt: "desc" })
-    query.select("_id name address openingTime image isAvailable");
-    const restaurants = await query.lean().exec();
-    if (!restaurants.length) {
-        return res
-            .status(200)
-            .json({ success: false, msg: `Restaurants not found` });
+    try {
+        const query = RestaurantModel.find({ idOwner: ObjectId(req.userId) });
+        query.sort({ createdAt: "desc" })
+        query.select("_id name address openingTime image isAvailable");
+        const restaurants = await query.lean().exec();
+        if (!restaurants.length) {
+            return res
+                .status(200)
+                .json({ success: false, msg: `Restaurants not found` });
+        }
+        return res.status(200).json({ success: true, msg: restaurants });
+    } catch (error) {
+        return res.status(400).json({ success: false, msg: error });
     }
-    return res.status(200).json({ success: true, msg: restaurants });
 };
 
 const updateRestaurant = async (req, res, next) => {
-    if (!isObjectIdOrHexString(req.params.restaurantId)) {
-        return res.status(400).json({
-            success: false,
-            msg: "Invalid ID.",
-        });
-    }
+    try {
+        if (!isObjectIdOrHexString(req.params.restaurantId)) {
+            return res.status(400).json({
+                success: false,
+                msg: "Invalid ID.",
+            });
+        }
 
-    const body = req.body;
+        const body = req.body;
 
-    if (!body || !body.openingTime) {
-        return res.status(400).json({
-            success: false,
-            msg: "You must provide body and opening time to update",
-        });
-    }
+        if (!body || !body.openingTime) {
+            return res.status(400).json({
+                success: false,
+                msg: "You must provide body and opening time to update",
+            });
+        }
 
-    for (const key in body.openingTime) {
-        if (Object.hasOwnProperty.call(body.openingTime, key)) {
-            const closingTime = moment(body.openingTime[key].to, "HH:mm");
-            const openingTime = moment(body.openingTime[key].from, "HH:mm");
-            if (closingTime.isSameOrBefore(openingTime)) {
-                return res.status(400).json({
-                    success: false,
-                    msg: "Čas otevření nesmí být starší než zavření",
-                });
+        for (const key in body.openingTime) {
+            if (Object.hasOwnProperty.call(body.openingTime, key)) {
+                const closingTime = moment(body.openingTime[key].to, "HH:mm");
+                const openingTime = moment(body.openingTime[key].from, "HH:mm");
+                if (closingTime.isSameOrBefore(openingTime)) {
+                    return res.status(400).json({
+                        success: false,
+                        msg: "Čas otevření nesmí být starší než zavření",
+                    });
+                }
             }
         }
-    }
 
-    const foundRestaurants = await RestaurantModel.find({
-        idOwner: ObjectId(body.idOwner),
-        name: body.name
-    }).select("name").exec();
+        const foundRestaurants = await RestaurantModel.find({
+            idOwner: ObjectId(body.idOwner),
+            name: body.name
+        }).select("name").exec();
 
-    if (foundRestaurants.length > 1) {
-        return res.status(400).json({
-            success: false,
-            msg: "Restaurant " + foundRestaurants[0].name + " already exists!",
-        });
-    }
-
-    try {
+        if (foundRestaurants.length > 1) {
+            return res.status(400).json({
+                success: false,
+                msg: "Restaurant " + foundRestaurants[0].name + " already exists!",
+            });
+        }
         const restaurant = await RestaurantModel.findOneAndUpdate({ _id: ObjectId(req.params.restaurantId), idOwner: ObjectId(req.userId) }, body, { new: true, runValidators: true });
 
         if (!restaurant) {
@@ -121,61 +123,73 @@ const updateRestaurant = async (req, res, next) => {
 };
 
 const deleteRestaurant = async (req, res) => {
-    if (!isObjectIdOrHexString(req.params.restaurantId)) {
-        return res.status(400).json({
-            success: false,
-            msg: "Invalid ID.",
-        });
-    }
-    const restaurant = await RestaurantModel.findOneAndDelete({ _id: ObjectId(req.params.restaurantId), idOwner: ObjectId(req.userId) });
-    if (!restaurant) {
-        return res
-            .status(404)
-            .json({ success: false, msg: `Restaurant not found` });
-    }
+    try {
+        if (!isObjectIdOrHexString(req.params.restaurantId)) {
+            return res.status(400).json({
+                success: false,
+                msg: "Invalid ID.",
+            });
+        }
+        const restaurant = await RestaurantModel.findOneAndDelete({ _id: ObjectId(req.params.restaurantId), idOwner: ObjectId(req.userId) });
+        if (!restaurant) {
+            return res
+                .status(404)
+                .json({ success: false, msg: `Restaurant not found` });
+        }
 
-    return res
-        .status(200)
-        .json({
-            success: true,
-            msg: "Deleted restaurant " + restaurant.name + " successfully!",
-        });
+        return res
+            .status(200)
+            .json({
+                success: true,
+                msg: "Deleted restaurant " + restaurant.name + " successfully!",
+            });
+    } catch (error) {
+        return res.status(400).json({ success: false, msg: error });
+    }
 };
 
 const getRestaurantByID = async (req, res) => {
-    const query = RestaurantModel.findOne({ _id: ObjectId(req.params.restaurantId), idOwner: ObjectId(req.userId) });
-    query.select("_id name address openingTime image api isAvailable");
-    const restaurant = await query.lean().exec();
-    if (!restaurant) {
-        return res
-            .status(200)
-            .json({ success: false, msg: `Restaurants not found` });
+    try {
+        const query = RestaurantModel.findOne({ _id: ObjectId(req.params.restaurantId), idOwner: ObjectId(req.userId) });
+        query.select("_id name address openingTime image api isAvailable");
+        const restaurant = await query.lean().exec();
+        if (!restaurant) {
+            return res
+                .status(200)
+                .json({ success: false, msg: `Restaurants not found` });
+        }
+        return res.status(200).json({ success: true, msg: restaurant });
+    } catch (error) {
+        return res.status(400).json({ success: false, msg: error });
     }
-    return res.status(200).json({ success: true, msg: restaurant });
 };
 
 const getRestaurantTransactions = async (req, res) => {
-    const query = TransactionModel.find({ idRestaurant: ObjectId(req.params.restaurantId) });
-    query.sort({ createdAt: 'desc' });
+    try {
+        const query = TransactionModel.find({ idRestaurant: ObjectId(req.params.restaurantId) });
+        query.sort({ createdAt: 'desc' });
 
-    const transactions = await query.lean().exec();
-    if (!transactions) {
-        return res
-            .status(200)
-            .json({ success: false, msg: `Transactions not found` });
+        const transactions = await query.lean().exec();
+        if (!transactions) {
+            return res
+                .status(200)
+                .json({ success: false, msg: `Transactions not found` });
+        }
+        return res.status(200).json({ success: true, msg: transactions });
+    } catch (error) {
+        return res.status(400).json({ success: false, msg: error });
     }
-    return res.status(200).json({ success: true, msg: transactions });
 };
 
 const getSelf = async (req, res, next) => {
-    const authorizedMerchantId = req.userId;
-    if (!isObjectIdOrHexString(authorizedMerchantId)) {
-        return res.status(400).json({
-            success: false,
-            msg: "Invalid ID.",
-        });
-    }
     try {
+        const authorizedMerchantId = req.userId;
+        if (!isObjectIdOrHexString(authorizedMerchantId)) {
+            return res.status(400).json({
+                success: false,
+                msg: "Invalid ID.",
+            });
+        }
         const merchant = await MerchantModel.findById(authorizedMerchantId);
 
         if (!merchant) {
@@ -201,47 +215,47 @@ const getSelf = async (req, res, next) => {
 };
 
 const updateMerchant = async (req, res, next) => {
-    const merchantId = req.userId;
-    const body = req.body;
-    if (!body || !body.password) {
-        return res.status(400).json({
-            success: false,
-            msg: "You must provide a body and password to update",
-        });
-    }
-
-    const merchant = await MerchantModel.findById(merchantId);
-
-    if (!merchant) {
-        return res.status(404).json({
-            success: false,
-            msg: "Merchant not found"
-        });
-    }
-
-    const isPasswordValid = await bcrypt.compare(body.password, merchant.password);
-
-    if (!isPasswordValid) {
-        return res.status(400).json({
-            success: false, msg: {
-                password: 'Nesprávné heslo'
-            }
-        });
-    }
-
-    delete body['password'];
-
-    const paymentGates = merchant.paymentGates || {};
-
-    if (body.paymentGates && body.paymentGates.comgate) {
-        paymentGates.comgate = body.paymentGates.comgate;
-    }
-
-    if (body.paymentGates && body.paymentGates.csob) {
-        paymentGates.csob = body.paymentGates.csob;
-    }
-
     try {
+        const merchantId = req.userId;
+        const body = req.body;
+        if (!body || !body.password) {
+            return res.status(400).json({
+                success: false,
+                msg: "You must provide a body and password to update",
+            });
+        }
+
+        const merchant = await MerchantModel.findById(merchantId);
+
+        if (!merchant) {
+            return res.status(404).json({
+                success: false,
+                msg: "Merchant not found"
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(body.password, merchant.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                success: false, msg: {
+                    password: 'Nesprávné heslo'
+                }
+            });
+        }
+
+        delete body['password'];
+
+        const paymentGates = merchant.paymentGates || {};
+
+        if (body.paymentGates && body.paymentGates.comgate) {
+            paymentGates.comgate = body.paymentGates.comgate;
+        }
+
+        if (body.paymentGates && body.paymentGates.csob) {
+            paymentGates.csob = body.paymentGates.csob;
+        }
+
         const updatedMerchant = await MerchantModel.findByIdAndUpdate(merchantId, Object.keys(paymentGates).length === 0 ? body : { paymentGates: paymentGates }, { runValidators: true, new: true });
         if (typeof body.isAvailable === 'boolean' && !body.isAvailable && merchant.isAvailable) {
             await RestaurantModel.updateMany({ idOwner: ObjectId(merchantId) }, { isAvailable: false });
@@ -257,44 +271,44 @@ const updateMerchant = async (req, res, next) => {
 };
 
 const updatePassword = async (req, res, next) => {
-    const merchantId = req.userId;
-    const body = req.body;
-
-    if (!body || !body.currentPassword || !body.newPassword || !body.confirmNewPassword) {
-        return res.status(400).json({
-            success: false,
-            msg: "You must provide a body and password to update",
-        });
-    }
-
-    if (body.newPassword !== body.confirmNewPassword) {
-        return res.status(400).json({
-            success: false,
-            msg: { confirmNewPassword: "New passwords not matching" },
-        });
-    }
-
-    const merchant = await MerchantModel.findById(merchantId);
-
-    if (!merchant) {
-        return res.status(404).json({
-            success: false,
-            msg: "Merchant not exist"
-        });
-    }
-
-    const isPasswordValid = await bcrypt.compare(body.currentPassword, merchant.password);
-
-    if (!isPasswordValid) {
-        return res.status(400).json({
-            success: false,
-            msg: {
-                currentPassword: 'Incorrect password'
-            }
-        });
-    }
-
     try {
+        const merchantId = req.userId;
+        const body = req.body;
+
+        if (!body || !body.currentPassword || !body.newPassword || !body.confirmNewPassword) {
+            return res.status(400).json({
+                success: false,
+                msg: "You must provide a body and password to update",
+            });
+        }
+
+        if (body.newPassword !== body.confirmNewPassword) {
+            return res.status(400).json({
+                success: false,
+                msg: { confirmNewPassword: "New passwords not matching" },
+            });
+        }
+
+        const merchant = await MerchantModel.findById(merchantId);
+
+        if (!merchant) {
+            return res.status(404).json({
+                success: false,
+                msg: "Merchant not exist"
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(body.currentPassword, merchant.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({
+                success: false,
+                msg: {
+                    currentPassword: 'Incorrect password'
+                }
+            });
+        }
+
         merchant.password = body.newPassword;
         await merchant.save();
         return res.status(200).json({
